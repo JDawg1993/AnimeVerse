ea55d2b8ecbf7f1b3c4451973598fd7c6cbc042f
UnknownJDawg <j0rdan.d3an1993@gmail.com>
2024-06-20 00:41:35 +0100

    Created Input system, ConnectionButtons  and Player Prefab

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+[CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader")]
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    public event Action<Vector2> MoveEvent;
+
+
+    private Controls controls;
+
+    private void OnEnable()
+    {
+        if (controls == null)
+        {
+            controls = new Controls();
+            controls.Player.SetCallbacks(this);
+        }
+
+        controls.Player.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+}
